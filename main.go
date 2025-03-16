47832bc4486dc5dd75ef871d6bc4a68c86bfa4b1
Jaylen Douglas <douglas.jaylen@gmail.com>
2025-03-16 10:28:40 -0400

    Breaking change update; uses arguments instead of options.

--- main.go
+++ main.go
@@ -7,39 +7,49 @@ import (
 	"os/exec"
 )
 
-func main() {
-	// Define usage
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "Options:\n")
-		flag.PrintDefaults()
-	}
-	// Define command line args
-	ipVersion := flag.Int("v", 4, "Which IP version to display")
-	// Parse command line arguments
-	flag.Parse()
-	// Validate IP version
-	if *ipVersion != 4 && *ipVersion != 6 {
-		fmt.Fprintf(os.Stderr, "[error] IP version must be 4 or 6. You provided: %d\n", *ipVersion)
-		flag.Usage()
-		os.Exit(1)
-	}
-	// Execute operation
-	getIPAddress(ipVersion)
-	// Exit successfully
-	os.Exit(0)
-}
-
-func getIPAddress(ipVersion *int) {
+func getIPv4() {
 	var url string = "https://ifconfig.me" // HTTPS
-	if *ipVersion == 6 {
-		url = "http://ifconfig.me" // HTTP
-	}
 	cmd := exec.Command("curl", "-4", url)
 	output, err := cmd.Output()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to fetch IPv%d", ipVersion)
+		fmt.Fprintf(os.Stderr, "Unable to fetch IPv4")
 		os.Exit(2)
 	}
 	fmt.Println(string(output))
+}
+
+func getIPv6() {
+	var url string = "http://ifconfig.me" // HTTP
+	cmd := exec.Command("curl", "-4", url)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to fetch IPv6")
+		os.Exit(2)
+	}
+	fmt.Println(string(output))
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: myip v4 (or v6)\n")
+		os.Exit(1)
+	}
+	// Parse command line arguments
+	flag.Parse()
+	// Work with arguments
+	args := flag.Args()
+	if len(args) != 1 {
+		flag.Usage()
+	}
+	ipVersion := flag.Arg(0)
+	switch ipVersion {
+	case "v4":
+		getIPv4()
+		break
+	case "v6":
+		getIPv6()
+		break
+	default:
+		flag.Usage()
+	}
 }
